b3cdc878efe452f2271edd5ac6ae66baecd6a73e
Alex Van <alexhtwen@gmail.com>
2020-08-21 03:17:11 +0800

    almost final

--- app.js
+++ app.js
@@ -2,13 +2,13 @@
 let express = require('express');
 let firebase = require('firebase')
 let firebaseConfig = {
-    apiKey: "AIzaSyBw_IQD3e9D_YmfNAgLwg6ky8O87TKv6cA",
-    authDomain: "nodejs-test-9e44f.firebaseapp.com",
-    databaseURL: "https://nodejs-test-9e44f.firebaseio.com",
-    projectId: "nodejs-test-9e44f",
-    storageBucket: "nodejs-test-9e44f.appspot.com",
-    messagingSenderId: "1021821736753",
-    appId: "1:1021821736753:web:c80cae393406a15bcf8210"
+   apiKey: "AIzaSyBw_IQD3e9D_YmfNAgLwg6ky8O87TKv6cA",
+   authDomain: "nodejs-test-9e44f.firebaseapp.com",
+   databaseURL: "https://nodejs-test-9e44f.firebaseio.com",
+   projectId: "nodejs-test-9e44f",
+   storageBucket: "nodejs-test-9e44f.appspot.com",
+   messagingSenderId: "1021821736753",
+   appId: "1:1021821736753:web:c80cae393406a15bcf8210"
 };
 
 // Initialize Firebase
@@ -18,109 +18,135 @@ let app = express();
 app.use(express.static('./public'));
 app.set('view engine', 'ejs');
 app.get('/', async (req, res) => {
-    //let data = await db.collection('classA').get();
-    //let userArr = []
-    // data.forEach((doc) => {
-    //     console.log(doc.data().name)
-    //     userArr.push(doc.data().name);
-    // })
+   //let data = await db.collection('classA').get();
+   //let userArr = []
+   // data.forEach((doc) => {
+   //     console.log(doc.data().name)
+   //     userArr.push(doc.data().name);
+   // })
 
-    // res.render('default', {
-    res.render('home', {
-        header: 'node.js期末作業--Alex',
-        title: '本網站可用的routes:',
-        // users: ['Alex', 'Marrianne', 'Rebecca', 'Thomas', 'Asing']
-        routes: ['/ (本頁)', '/alex (Alex專頁)', '/firebase-test (??)', '/who/{name} (列出特定會員)', '/API/addMember (新增會員)', '/API/deleteMember (刪除會員)']
-        //users: userArr
-    });
+   // res.render('default', {
+   res.render('home', {
+      header: 'node.js期末作業--Alex',
+      title: '本網站可用的routes:',
+      // users: ['Alex', 'Marrianne', 'Rebecca', 'Thomas', 'Asing']
+      routes: ['/ (本頁)', '/class (node.js班學員資料頁)', '/greet/{name} (向{name}打個招呼)', ' /db-test (列出collection classA所有資料，無GUI介面)', '/search/{name} (列出classA中所有name為{name}的資料)', '/alex (Alex專頁)']
+      //users: userArr
+   });
 });
 
 
 app.get("/alex", (req, res) => {
-    res.send("<h1>Hey, I am Alex.</h1>")
+   res.send("<br><br><br><h1><center><font color=red>Hey, I am Alex.</font></center></h1><br><br><h2><center><font color=tomato>Welcome to my page.</font></center></h2>")
 })
 
-app.get("/firebase-test", async (req, res) => {
-    let html = '';
-    let data = await db.collection('classA').get();
-    data.forEach(doc => {
-        console.log(doc.data());
-        html += `<div>${doc.id}: name = ${doc.data().name} age = ${doc.data().age}</div>`;
-    });
-    res.send(html)
+app.get("/db-test", async (req, res) => {
+   let html = '';
+   let data = await db.collection('classA').get();
+   data.forEach(doc => {
+      console.log(doc.data());
+      // html += `<div><h2>id=${doc.id}, name=${doc.data().name}, gender=${doc.data().gender}, age=${doc.data().age}</h2></div>`;
+      html += `<div><h2>id=${doc.id}&nbsp;&nbsp;&nbsp;&nbsp;name=${doc.data().name}&nbsp;&nbsp;&nbsp;&nbsp;gender=${doc.data().gender}&nbsp;&nbsp;&nbsp;&nbsp;age=${doc.data().age}</h2></div>`;
+   });
+   res.send(html)
 })
 
-app.get("/classA_backend", async (req, res) => {
-    let data = await db.collection('classA').get();
-    userArr = []
-    data.forEach((doc) => {
-        userArr.push({
-            id: doc.id,
-            name: doc.data().name,
-            age: doc.data().age,
-            gender: doc.data().gender
-        })
-    })
-    res.render('classA', {
-        users: userArr
-    })
+// app.get("/classA_backend", async (req, res) => {
+app.get("/class", async (req, res) => {
+   let data = await db.collection('classA').get();
+   userArr = []
+   data.forEach((doc) => {
+      userArr.push({
+         id: doc.id,
+         name: doc.data().name,
+         age: doc.data().age,
+         gender: doc.data().gender
+      })
+   })
+   res.render('classA', {
+      users: userArr
+   })
 })
 
 app.get("/classA_frontend", (req, res) => {
-    let options = {
-        root: __dirname + "/public",
-        dotfiles: 'ignore'
-    }
-    console.log(__dirname + "/public");
-    res.sendFile("/classA.html", options);
+   let options = {
+      root: __dirname + "/public",
+      dotfiles: 'ignore'
+   }
+   console.log(__dirname + "/public");
+   res.sendFile("/classA.html", options);
 })
 
-app.get('/who/:name', (req, res) => {
-    let name = req.params.name;
-    res.send(`This is ${name}`);
+app.get('/greet/:name', (req, res) => {
+   let name = req.params.name;
+   res.send(`<h1>How are you doing? ${name}</h1>`);
 });
 
-app.get('/API/deleteMember', (req, res) => {
-    db.collection('classA').doc(req.query.id).delete();
-    // console.log(req.query.id);
-    // res.send(`delete Member id = ${req.query.id}!`)
-    let prompt = `Student id: ${req.query.id} deleted.`;
 
-    // location.reload();
-    console.log(`log: ${prompt}`);
-    res.send(`${prompt}`);
+app.get("/search/:name", async (req, res) => {
+   let html = '';
+   let name = req.params.name;
+
+   // Alex：以下這列為何不行？
+   // let data = db.collection('classA').where("name", "==", name);
+   let data = await db.collection('classA').get();
+
+   userArr = []
+   data.forEach((doc) => {
+      let thisName = doc.data().name
+      if (thisName == name) {
+         console.log(doc.data());
+         // html += `<div><h2>id=${doc.id}, name=${doc.data().name}, gender=${doc.data().gender}, age=${doc.data().age}</h2></div>`;
+         html += `<div><h2>id=${doc.id}&nbsp;&nbsp;&nbsp;&nbsp;name=${doc.data().name}&nbsp;&nbsp;&nbsp;&nbsp;gender=${doc.data().gender}&nbsp;&nbsp;&nbsp;&nbsp;age=${doc.data().age}</h2></div>`;
+      }
+   })
+   res.send(html)
+});
+
+
+
+
+app.get('/API/deleteStudent', (req, res) => {
+   db.collection('classA').doc(req.query.id).delete();
+   // console.log(req.query.id);
+   // res.send(`delete Student id = ${req.query.id}!`)
+   let prompt = `Student id: ${req.query.id} deleted.`;
+
+   // location.reload();
+   console.log(`log: ${prompt}`);
+   res.send(`${prompt}`);
 })
 
-app.get('/API/addMember', (req, res) => {
-    let thisMember = {
-        name: req.query.name,
-        gender: req.query.age,
-        age: req.query.gender
-    }
-    db.collection('classA').add(thisMember);
-    console.log(`log: Member ${thisMember.name} successfully added.`);
-    res.send(`Member ${thisMember.name} successfully added.`);
+app.get('/API/addStudent', (req, res) => {
+   let thisStudent = {
+      name: req.query.name,
+      gender: req.query.age,
+      age: req.query.gender
+   }
+   db.collection('classA').add(thisStudent);
+   console.log(`log: Student ${thisStudent.name} successfully added.`);
+   res.send(`Student ${thisStudent.name} successfully added.`);
 })
 
 
-// app.get('/API/addMember', (req, res) => {
+// app.get('/API/addStudent', (req, res) => {
 //     db.collection('classA').add({
 //         name: req.query.name,
 //         gender: req.query.age,
 //         age: req.query.gender
 //     });
-//     console.log("Add member !!");
-//     res.send("Add member success!");
+//     console.log("Add student !!");
+//     res.send("Add student success!");
 // })
 
 
 
 app.get('*', (req, res) => {
-    res.send('No Content');
+   res.send('<h1>此參數無效。</h1>');
 });
 
 let port = process.env.PORT || 9876;
 
 app.listen(port, () => {
-    console.log(`Hey, I am listening on port ${port}.`);
+   console.log(`Hey, I am listening on port ${port}.`);
 });
--- public/backend_setEvent.js
+++ public/backend_setEvent.js
@@ -1,19 +1,20 @@
-const cross = document.querySelectorAll('#cross-family')
+const cross = document.querySelectorAll('#delete-me')
 const form = document.querySelector("#add-students-form");
 for (let i = 0; i < cross.length; i++) {
     cross[i].addEventListener('click', async (test) => {
         test.stopPropagation();
         let id = cross[i].getAttribute('cross-id');
         console.log(id);
-        let apiUrl = `http://127.0.0.1:9876/API/deleteMember?id=${id}`
+        let apiUrl = `http://127.0.0.1:9876/API/deleteStudent?id=${id}`
         let res = await fetch(apiUrl, {
             method: 'GET'
         });
         let text = await res.text();
         console.log(text);
-        console.log("in the cross, id = ", id);
+        console.log(`id:${id} being deleted...`);
         // call delete api
-        location.reload();
+        // location.reload(true);
+        res.redirect('back');
     });
 }
 // add data
@@ -24,7 +25,7 @@ form.addEventListener('submit', async (e) => {
     name = form.name.value;
     gender = form.gender.value;
     age = form.age.value;
-    let apiUrl = `http://127.0.0.1:9876/API/addMember?name=${name}&gender=${gender}&age=${age}`
+    let apiUrl = `http://127.0.0.1:9876/API/addStudent?name=${name}&gender=${gender}&age=${age}`
     let res = await fetch(apiUrl, {
         method: 'GET'
     });
